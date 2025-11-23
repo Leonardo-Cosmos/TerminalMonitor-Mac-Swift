dfce2af89b06479381107b0f21d0b9ca668cc25f
Leonardo-Cosmos <kylin_cl@hotmail.com>
2025-11-23 17:23:35 +0800

    Implement conditional style

--- TerminalMonitor/Views/TextStyleConditionView.swift
+++ TerminalMonitor/Views/TextStyleConditionView.swift
@@ -6,24 +6,68 @@
 //
 
 import SwiftUI
+import Combine
 
 struct TextStyleConditionView: View {
     
     @ObservedObject var viewModel: TextStyleConditionViewModel
     
-    @State var conditionDescription: String = ""
-    
     var body: some View {
         VStack {
             GroupBox(label: EmptyView()) {
                 HStack {
-                    Button(viewModel.condition.conditionDescription) {
-                        ConditionDetailWindowController.openWindow(for: $viewModel.condition) { condition in
-                            conditionDescription = condition.conditionDescription
-                        }
+                    Button("Edit", systemImage: "pencil") {
+                        ConditionDetailWindowController.openWindow(for: $viewModel.condition)
                     }
-                    .onAppear() {
-                        conditionDescription = viewModel.condition.conditionDescription
+                    .labelStyle(.iconOnly)
+                    
+                    Text("Condition")
+                    
+                    Text(viewModel.condition.conditionDescription)
+                        .padding(4)
+                        .overlay(RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color.gray.opacity(1)))
+                    
+                    HStack {
+                        
+                        SymbolButtonToggle(
+                            toggle: Binding(
+                                get: { viewModel.condition.isInverted },
+                                set: { viewModel.condition.isInverted = $0 }
+                            ),
+                            toggleOnSystemImage: "minus.circle.fill",
+                            toggleOnSystemColor: .red,
+                            toggleOnHelpTextKey: NSLocalizedString("Matching is Inverted", comment: ""),
+                            toggleOffSystemImage: "largecircle.fill.circle",
+                            toggleOffSystemColor: .green,
+                            toggleOffHelpTextKey: NSLocalizedString("Matching is not Inverted", comment: "")
+                        )
+                        
+                        SymbolButtonToggle(
+                            toggle: Binding(
+                                get: { viewModel.condition.defaultResult },
+                                set: { viewModel.condition.defaultResult = $0 }
+                            ),
+                            toggleOnSystemImage: "star.fill",
+                            toggleOnSystemColor: .yellow,
+                            toggleOnHelpTextKey: NSLocalizedString("Default to True when the Field is not Found", comment: ""),
+                            toggleOffSystemImage: "star",
+                            toggleOffSystemColor: .yellow,
+                            toggleOffHelpTextKey: NSLocalizedString("Default to False when the Field is not Found", comment: "")
+                        )
+                        
+                        SymbolButtonToggle(
+                            toggle: Binding(
+                                get: { viewModel.condition.isDisabled },
+                                set: { viewModel.condition.isDisabled = $0 }
+                            ),
+                            toggleOnSystemImage: "pause.circle",
+                            toggleOnSystemColor: .red,
+                            toggleOnHelpTextKey: NSLocalizedString("This Condition is Disabled", comment: ""),
+                            toggleOffSystemImage: "dot.circle",
+                            toggleOffSystemColor: .green,
+                            toggleOffHelpTextKey: NSLocalizedString("This Condition is Enabled", comment: "")
+                        )
                     }
                     
                     Spacer()
@@ -44,10 +88,16 @@ class TextStyleConditionViewModel: Identifiable, ObservableObject {
     
     @Published var condition: Condition
     
+    private var cancellables = Set<AnyCancellable>()
+    
     init(id: UUID, style: TextStyleViewModel, condition: Condition) {
         self.id = id
         self.style = style
         self.condition = condition
+        
+        condition.objectWillChange
+            .sink { [weak self] _ in self?.objectWillChange.send() }
+            .store(in: &cancellables)
     }
     
     convenience init(style: TextStyleViewModel, condition: Condition) {
